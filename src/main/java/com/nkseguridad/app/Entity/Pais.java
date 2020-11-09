5d3923a56dce28e642574fefd2c9166e515a0e8b
aaraujocl <a.araujo.cl@hotmail.com>
2020-11-09 17:13:09 -0500

    Create Pais.java

---
+++ src/main/java/com/nkseguridad/app/Entity/Pais.java
@@ -0,0 +1,67 @@
+package com.nkseguridad.app.Entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pais", schema = "public")
+public class Pais implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7761843299255344427L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(name = "codigo",unique = true, length = 4, insertable = false, updatable = false)
+	private String codigo;
+	
+	@Column(name = "nombre", length = 120, insertable = false, updatable = false)
+	private String nombre;
+	
+	@Column(name = "satus", length = 10, insertable = false, updatable = false)
+	private String Status;
+
+	
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+	
+
+}
