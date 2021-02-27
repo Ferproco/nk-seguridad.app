fa9a40a8dceca3ac3c043b0727928f0ef184a631
maiteresa <72674731+maiteresa@users.noreply.github.com>
2021-02-27 17:18:11 -0500

    Update Mapper.java

--- src/main/java/com/nkseguridad/app/Mapper/Mapper.java
+++ src/main/java/com/nkseguridad/app/Mapper/Mapper.java
@@ -3,18 +3,28 @@ package com.nkseguridad.app.Mapper;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.nkseguridad.app.Entity.Kardex;
 import com.nkseguridad.app.Model.MKardex;
+import com.nkseguridad.app.Service.IArticuloService;
 
 @Component("mapper")
 public class Mapper {
 	
-	public static List<MKardex> convertirKardex(List<Kardex> kadexList){
+	
+	
+	public static List<MKardex> convertirKardex(List<Kardex> kadexList, IArticuloService IarticuloService){
 		
 		List<MKardex> lstKardexMapper = new ArrayList<>();
 		for (Kardex kardex: kadexList) {
+			try {
+				kardex.setArticulo(IarticuloService.findById(kardex.getArticulo_id()));
+			}
+			catch(Exception e){
+				System.out.print(e);
+			}
 			lstKardexMapper.add(new MKardex(kardex));
 		}
 		return lstKardexMapper;
